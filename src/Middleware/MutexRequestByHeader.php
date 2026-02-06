363a017f741c256fc8a66cac61e6346871b50314
chengj <chen.gj@qq.com>
2026-02-06 11:15:54 +0800

    mutex lock

--- src/Middleware/MutexRequestByHeader.php
+++ src/Middleware/MutexRequestByHeader.php
@@ -8,28 +8,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Oh86\Http\Exceptions\ErrorCodeException;
 
-class MutexRequestByArg
+class MutexRequestByHeader
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure $next
-     * @param  string  $argName
+     * @param  string  $headerName
      * @param  int  $waitSeconds
      * @param  int  $lockSeconds
      * @param  null|string  $lockKey    缺省为路由uri
      * @return mixed
      */
-    public function handle(Request $request, Closure $next, string $argName, int $waitSeconds = 10, int $lockSeconds = 300, $lockKey = null)
+    public function handle(Request $request, Closure $next, string $headerName, int $waitSeconds = 10, int $lockSeconds = 300, $lockKey = null)
     {
         $lockKey ??= $request->route()->uri();
-        $val = $request->$argName;
+        $val = $request->header($headerName);
         if (is_array($val)) {
             $val = json_encode($val);
         }
 
-        $lockName = "mutexRequest:$lockKey:$argName:$val";
+        $lockName = "mutexRequest:$lockKey:$headerName:$val";
         $lock = Cache::lock($lockName, $lockSeconds);
 
         $isLocked = false;
